4c28463c4668bfce624eff2ffb72d582a18c8d60
Robert Machmer <robert.machmer@gmail.com>
2014-10-18 16:08:36 +0200

    Add debug information (FPS, frametime, memory).

--- main.lua
+++ main.lua
@@ -7,6 +7,10 @@ end
 
 function love.draw()
     ScreenManager.draw();
+
+    love.graphics.print(string.format("FT: %.3f ms", 1000 * love.timer.getAverageDelta()), 10, love.window.getHeight() - 60);
+    love.graphics.print(string.format("FPS: %.3f fps", love.timer.getFPS()), 10, love.window.getHeight() - 40);
+    love.graphics.print(string.format("MEM: %.3f kb", collectgarbage("count")), 10, love.window.getHeight() - 20);
 end
 
 function love.update(dt)
