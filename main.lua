e62ce0f59114b3df743827ee445a3c15aca55d21
Robert Machmer <robert.machmer@gmail.com>
2015-01-18 03:41:40 +0100

    Add copyright information.

--- main.lua
+++ main.lua
@@ -1,3 +1,25 @@
+--==================================================================================================
+-- Copyright (C) 2014 - 2015 by Robert Machmer                                                     =
+--                                                                                                 =
+-- Permission is hereby granted, free of charge, to any person obtaining a copy                    =
+-- of this software and associated documentation files (the "Software"), to deal                   =
+-- in the Software without restriction, including without limitation the rights                    =
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell                       =
+-- copies of the Software, and to permit persons to whom the Software is                           =
+-- furnished to do so, subject to the following conditions:                                        =
+--                                                                                                 =
+-- The above copyright notice and this permission notice shall be included in                      =
+-- all copies or substantial portions of the Software.                                             =
+--                                                                                                 =
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR                      =
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,                        =
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE                     =
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER                          =
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,                   =
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN                       =
+-- THE SOFTWARE.                                                                                   =
+--==================================================================================================
+
 local ScreenManager = require('lib/ScreenManager');
 
 function love.load()
@@ -19,7 +41,3 @@ end
 function love.update(dt)
     ScreenManager.update(dt);
 end
-
---==================================================================================================
--- Created 01.10.14 - 11:01                                                                        =
---==================================================================================================
