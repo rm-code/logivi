c5a534faa413dce1abf89a73eb8969fecf0353d8
Robert Machmer <robert.machmer@gmail.com>
2015-04-11 03:52:38 +0200

    Move camera code outside of the MainScreen class

--- src/screens/MainScreen.lua
+++ src/screens/MainScreen.lua
@@ -22,7 +22,7 @@
 
 local Screen = require('lib.screenmanager.Screen');
 local LogReader = require('src.LogReader');
-local Camera = require('lib.camera.Camera');
+local Camera = require('src.ui.CamWrapper');
 local ConfigReader = require('src.conf.ConfigReader');
 local AuthorManager = require('src.AuthorManager');
 local FileManager = require('src.FileManager');
@@ -37,25 +37,9 @@ local InputHandler = require('src.InputHandler');
 
 local LOG_FILE = 'log.txt';
 
-local CAMERA_ROTATION_SPEED = 0.6;
-local CAMERA_TRANSLATION_SPEED = 400;
-local CAMERA_TRACKING_SPEED = 2;
-local CAMERA_ZOOM_SPEED = 0.6;
-local CAMERA_MAX_ZOOM = 0.05;
-local CAMERA_MIN_ZOOM = 2;
-
 -- ------------------------------------------------
 -- Controls
 -- ------------------------------------------------
-
-local camera_zoomIn;
-local camera_zoomOut;
-local camera_rotateL;
-local camera_rotateR;
-local camera_n;
-local camera_s;
-local camera_e;
-local camera_w;
 
 local toggleAuthors;
 local toggleFilePanel;
@@ -83,12 +67,7 @@ function MainScreen.new()
     local self = Screen.new();
 
     local graph;
-
     local camera;
-    local cx, cy;
-    local ox, oy;
-    local zoom = 1;
-
     local filePanel;
     local timeline;
 
@@ -97,73 +76,11 @@ function MainScreen.new()
     -- ------------------------------------------------
 
     ---
-    -- Processes camera related controls and updates the camera.
-    -- @param cx - The current x-position the camera is looking at.
-    -- @param cy - The current y-position the camera is looking at.
-    -- @param ox - The current offset of the camera on the x-axis.
-    -- @param oy - The current offset of the camera on the y-axis.
-    -- @param dt
-    --
-    local function updateCamera(cx, cy, ox, oy, dt)
-        -- Zoom.
-        if InputHandler.isDown(camera_zoomIn) then
-            zoom = zoom + CAMERA_ZOOM_SPEED * dt;
-        elseif InputHandler.isDown(camera_zoomOut) then
-            zoom = zoom - CAMERA_ZOOM_SPEED * dt;
-        end
-        zoom = math.max(CAMERA_MAX_ZOOM, math.min(zoom, CAMERA_MIN_ZOOM));
-        camera:zoomTo(zoom);
-
-        -- Rotation.
-        if InputHandler.isDown(camera_rotateL) then
-            camera:rotate(CAMERA_ROTATION_SPEED * dt);
-        elseif InputHandler.isDown(camera_rotateR) then
-            camera:rotate(-CAMERA_ROTATION_SPEED * dt);
-        end
-
-        -- Horizontal Movement.
-        local dx = 0;
-        if InputHandler.isDown(camera_w) then
-            dx = dx - dt * CAMERA_TRANSLATION_SPEED;
-        elseif InputHandler.isDown(camera_e) then
-            dx = dx + dt * CAMERA_TRANSLATION_SPEED;
-        end
-        -- Vertical Movement.
-        local dy = 0;
-        if InputHandler.isDown(camera_n) then
-            dy = dy - dt * CAMERA_TRANSLATION_SPEED;
-        elseif InputHandler.isDown(camera_s) then
-            dy = dy + dt * CAMERA_TRANSLATION_SPEED;
-        end
-
-        -- Take the camera rotation into account when calculating the new offset.
-        ox = ox + (math.cos(-camera.rot) * dx - math.sin(-camera.rot) * dy);
-        oy = oy + (math.sin(-camera.rot) * dx + math.cos(-camera.rot) * dy);
-
-        -- Gradually move the camera to the target position.
-        local gx, gy = graph:getCenter();
-        cx = cx - (cx - math.floor(gx + ox)) * dt * CAMERA_TRACKING_SPEED;
-        cy = cy - (cy - math.floor(gy + oy)) * dt * CAMERA_TRACKING_SPEED;
-        camera:lookAt(cx, cy);
-
-        return cx, cy, ox, oy;
-    end
-
-    ---
     -- Assigns keybindings loaded from the config file to a
     -- local variable for faster access.
     -- @param config
     --
     local function assignKeyBindings(config)
-        camera_zoomIn = config.keyBindings.camera_zoomIn;
-        camera_zoomOut = config.keyBindings.camera_zoomOut;
-        camera_rotateL = config.keyBindings.camera_rotateL;
-        camera_rotateR = config.keyBindings.camera_rotateR;
-        camera_n = config.keyBindings.camera_n;
-        camera_s = config.keyBindings.camera_s;
-        camera_e = config.keyBindings.camera_e;
-        camera_w = config.keyBindings.camera_w;
-
         toggleAuthors = config.keyBindings.toggleAuthors;
         toggleFilePanel = config.keyBindings.toggleFileList;
         toggleLabels = config.keyBindings.toggleLabels;
@@ -208,18 +125,18 @@ function MainScreen.new()
 
         AuthorManager.init(config.aliases, config.avatars, config.options.showAuthors);
 
+        -- Create the camera.
+        camera = Camera.new();
+        camera:assignKeyBindings(config);
+
         graph = Graph.new(config.options.edgeWidth, config.options.showLabels);
         graph:register(AuthorManager);
+        graph:register(camera);
 
         -- Initialise LogReader and register observers.
         LogReader.init(LOG_FILE, config.options.commitDelay, config.options.mode, config.options.autoplay);
         LogReader.register(AuthorManager);
         LogReader.register(graph);
-
-        -- Create the camera.
-        camera = Camera.new();
-        cx, cy = 0, 0;
-        ox, oy = 0, 0;
 
         -- Create panel.
         filePanel = Panel.new(0, 0, 150, 400);
@@ -230,8 +147,8 @@ function MainScreen.new()
 
     function self:draw()
         camera:draw(function()
-            graph:draw(camera.rot);
-            AuthorManager.drawLabels(camera.rot);
+            graph:draw(camera:getRotation());
+            AuthorManager.drawLabels(camera:getRotation());
         end);
 
         filePanel:draw(FileManager.draw);
@@ -249,7 +166,7 @@ function MainScreen.new()
         timeline:setCurrentCommit(LogReader.getCurrentIndex());
         timeline:setCurrentDate(LogReader.getCurrentDate());
 
-        cx, cy, ox, oy = updateCamera(cx, cy, ox, oy, dt);
+        camera:move(dt);
     end
 
     function self:quit()
