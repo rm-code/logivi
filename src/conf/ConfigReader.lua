a7dba9b751a60779c15b78fbe384585e23f8b9b1
Robert Machmer <robert.machmer@gmail.com>
2016-01-24 21:41:46 +0100

    Refactor ConfigReader.lua
    Update code styling and remove an unused function.

--- src/conf/ConfigReader.lua
+++ src/conf/ConfigReader.lua
@@ -15,7 +15,6 @@ local MISSING_VALUE_WARNING   = 'Seems like the loaded configuration file is mis
 -- Local Variables
 -- ------------------------------------------------
 
-local default;
 local config;
 
 -- ------------------------------------------------
@@ -26,17 +25,17 @@ local config;
 -- Checks if the settings file exists on the user's system.
 --
 local function hasConfigFile()
-    return love.filesystem.isFile(FILE_NAME);
+    return love.filesystem.isFile( FILE_NAME );
 end
 
 ---
 -- Creates a new settings file on the user's system based on the default template.
--- @param name - The file name to use for the config file.
--- @param default - The path to the default settings file.
+-- @param filename - The file name to use for the config file.
+-- @param templatePath - The path to the template settings file.
 --
-local function createConfigFile(name, default)
-    for line in love.filesystem.lines(default) do
-        love.filesystem.append(name, line .. '\r\n');
+local function createConfigFile( filename, templatePath )
+    for line in love.filesystem.lines( templatePath ) do
+        love.filesystem.append( filename, line .. '\r\n' );
     end
 end
 
@@ -44,48 +43,48 @@ end
 -- Tries to transform strings to their actual types if possible.
 -- @param value - The value to transform.
 --
-local function toType(value)
-    value = value:match('^%s*(.-)%s*$');
+local function toType( value )
+    value = value:match( '^%s*(.-)%s*$' );
     if value == 'true' then
         return true;
     elseif value == 'false' then
         return false;
-    elseif tonumber(value) then
-        return tonumber(value);
+    elseif tonumber( value ) then
+        return tonumber( value );
     else
         return value;
     end
 end
 
-local function loadFile(file)
-    local config = {};
+local function loadFile( filePath )
+    local loadedConfig = {};
     local section;
 
-    for line in love.filesystem.lines(file) do
-        if line == '' or line:find(';') == 1 then
+    for line in love.filesystem.lines( filePath ) do
+        if line == '' or line:find( ';' ) == 1 then
             -- Ignore comments and empty lines.
-        elseif line:match('^%[(%w*)%]$') then
+        elseif line:match( '^%[(%w*)%]$' ) then
             -- Create a new section.
-            local header = line:match('^%[(%w*)%]$');
-            config[header] = {};
-            section = config[header];
+            local header = line:match( '^%[(%w*)%]$' );
+            loadedConfig[header] = {};
+            section = loadedConfig[header];
         else
             -- Store values in the section.
-            local key, value = line:match('^([%g]+)%s-=%s-(.+)');
+            local key, value = line:match( '^([%g]+)%s-=%s-(.+)' );
 
             -- Store multiple values in a table.
-            if value and value:find(',') then
+            if value and value:find( ',' ) then
                 section[key] = {};
-                for val in value:gmatch('[^, ]+') do
-                    section[key][#section[key] + 1] = toType(val);
+                for val in value:gmatch( '[^, ]+' ) do
+                    section[key][#section[key] + 1] = toType( val );
                 end
             elseif value then
-                section[key] = toType(value);
+                section[key] = toType( value );
             end
         end
     end
 
-    return config;
+    return loadedConfig;
 end
 
 ---
@@ -93,42 +92,38 @@ end
 -- It checks if the file contains all the necessary sections and values. If it
 -- doesn't a warning is displayed and the default config will be used.
 -- @param default - The default file to use for comparison.
--- @param loaded - The settings file loaded from the user's system.
 --
-local function validateFile(default, loaded)
-    print('Validating configuration file ... ');
-    for skey, section in pairs(default) do
+local function validateFile( default )
+    print( 'Validating configuration file ... ' );
+    for skey, section in pairs( default ) do
 
         -- If loaded config file doesn't contain section return default.
-        if loaded[skey] == nil then
-            love.window.showMessageBox(INVALID_CONFIG_HEADER, string.format(MISSING_SECTION_WARNING, skey), 'warning', false);
+        if config[skey] == nil then
+            love.window.showMessageBox( INVALID_CONFIG_HEADER, string.format( MISSING_SECTION_WARNING, skey ), 'warning', false );
             return default;
         end
 
         -- If the loaded config file is missing a value, display warning and return default.
-        if type(section) == 'table' then
-            for vkey, _ in pairs(section) do
-                if loaded[skey][vkey] == nil then
-                    love.window.showMessageBox(INVALID_CONFIG_HEADER, string.format(MISSING_VALUE_WARNING, vkey, skey), 'warning', false);
+        if type( section ) == 'table' then
+            for vkey, _ in pairs( section ) do
+                if config[skey][vkey] == nil then
+                    love.window.showMessageBox( INVALID_CONFIG_HEADER, string.format( MISSING_VALUE_WARNING, vkey, skey ), 'warning', false );
                     return default;
                 end
             end
         end
     end
 
-    print('Done!');
-    return loaded;
+    print( 'Done!' );
 end
 
 ---
 -- Replaces backslashes in paths with forwardslashes.
--- @param The loaded config.
 --
-local function validateRepositoryPaths(config)
-    for project, path in pairs(config.repositories) do
-        config.repositories[project] = path:gsub('\\+', '/');
+local function validateRepositoryPaths()
+    for project, path in pairs( config.repositories ) do
+        config.repositories[project] = path:gsub( '\\+', '/' );
     end
-    return config;
 end
 
 -- ------------------------------------------------
@@ -136,28 +131,20 @@ end
 -- ------------------------------------------------
 
 function ConfigReader.init()
-    default = loadFile(TEMPLATE_PATH);
+    local default = loadFile( TEMPLATE_PATH );
 
     if not hasConfigFile() then
-        createConfigFile(FILE_NAME, TEMPLATE_PATH);
+        createConfigFile( FILE_NAME, TEMPLATE_PATH );
     end
 
     -- If the config hasn't been loaded yet, load and validate it.
     if not config then
-        config = loadFile(FILE_NAME);
-        config = validateFile(default, config);
-        config = validateRepositoryPaths(config);
+        config = loadFile( FILE_NAME );
+        validateFile( default, config );
+        validateRepositoryPaths();
     end
 
     return config;
-end
-
--- ------------------------------------------------
--- Getters
--- ------------------------------------------------
-
-function ConfigReader.getConfig(section)
-    return config[section];
 end
 
 -- ------------------------------------------------
