d7a017e91130945386bf022265738e374c786e68
Robert Machmer <robert.machmer@gmail.com>
2016-01-31 01:49:31 +0100

    Refactor InputHandler.lua

--- src/InputHandler.lua
+++ src/InputHandler.lua
@@ -2,29 +2,29 @@ local InputHandler = {};
 
 ---
 -- Determines if a key constant or if any in a table of key constants are down.
--- @param constant - The key constant or table of constants.
+-- @param constant (string) The key constant or table of constants.
 --
-function InputHandler.isDown(constant)
-    if type(constant) == 'table' then
-        for _, keyToCheck in ipairs(constant) do
-            if love.keyboard.isDown(keyToCheck) then
+function InputHandler.isDown( constant )
+    if type( constant ) == 'table' then
+        for _, keyToCheck in ipairs( constant ) do
+            if love.keyboard.isDown( keyToCheck ) then
                 return true;
             end
         end
         return false;
     else
-        return love.keyboard.isDown(constant);
+        return love.keyboard.isDown( constant );
     end
 end
 
 ---
 -- Determines if a key constant or if any in a table of key constants was pressed.
--- @param key - The key to check for.
--- @param constant - The key constant or table of constants.
+-- @param key      (string) The key to check.
+-- @param constant (string) The key constant or table of constants to check for.
 --
-function InputHandler.isPressed(key, constant)
-    if type(constant) == 'table' then
-        for _, keyToCheck in ipairs(constant) do
+function InputHandler.isPressed( key, constant )
+    if type( constant ) == 'table' then
+        for _, keyToCheck in ipairs( constant ) do
             if key == keyToCheck then
                 return true;
             end
