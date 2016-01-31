4548292765177f79bd4e119f41b3c404cd5256aa
Robert Machmer <robert.machmer@gmail.com>
2016-01-31 01:53:03 +0100

    Refactor Resources.lua

--- src/Resources.lua
+++ src/Resources.lua
@@ -8,13 +8,13 @@ local IMG_PATH  = 'res/img/';
 local FONT_PATH = 'res/fonts/';
 
 -- ------------------------------------------------
--- Local Variables
+-- Private Class Variables
 -- ------------------------------------------------
 
 local images = {};
 local fonts  = {
     default = {
-        [12] = love.graphics.newFont(12)
+        [12] = love.graphics.newFont( 12 )
     }
 };
 
@@ -24,26 +24,26 @@ local fonts  = {
 
 ---
 -- Loads an image or returns an already loaded image.
--- @param name - The name of the file to load.
+-- @param name (string) The name of the file to load.
 --
-function Resources.loadImage(name)
+function Resources.loadImage( name )
     if not images[name] then
-        images[name] = love.graphics.newImage(IMG_PATH .. name);
+        images[name] = love.graphics.newImage( IMG_PATH .. name );
     end
-    return images[name]
+    return images[name];
 end
 
 ---
 -- Loads a font or returns an already loaded font.
--- @param name - The name of the font to load.
--- @param size - The size of the font to load.
+-- @param name (string) The name of the font to load.
+-- @param size (number) The size of the font to load.
 --
-function Resources.loadFont(name, size)
+function Resources.loadFont( name, size )
     if not fonts[name] then
         fonts[name] = {};
-        fonts[name][size] = love.graphics.newFont(FONT_PATH .. name, size);
+        fonts[name][size] = love.graphics.newFont( FONT_PATH .. name, size );
     elseif not fonts[name][size] then
-        fonts[name][size] = love.graphics.newFont(FONT_PATH .. name, size);
+        fonts[name][size] = love.graphics.newFont( FONT_PATH .. name, size );
     end
     return fonts[name][size];
 end
