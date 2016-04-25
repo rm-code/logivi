f78afbc845d89078714327add5cf05009745607a
Robert Machmer <robert.machmer@gmail.com>
2016-04-25 10:07:48 +0200

    Refactor InputPanel.lua

--- src/screens/InputPanel.lua
+++ src/screens/InputPanel.lua
@@ -8,12 +8,15 @@ local RepositoryHandler = require('src.conf.RepositoryHandler');
 -- ------------------------------------------------
 
 local INFO_FONT  = Resources.loadFont( 'SourceCodePro-Medium.otf', 10 );
-local LABEL_FONT  = Resources.loadFont( 'SourceCodePro-Medium.otf', 13 );
+local LABEL_FONT = Resources.loadFont( 'SourceCodePro-Medium.otf', 13 );
+
+local PANEL_WIDTH  = 280;
+local PANEL_HEIGHT =  60;
+local MAX_LENGTH   =  30;
 
 local BG_COLOR = { 60, 60, 60 };
-local MAX_LENGTH = 30;
-local PANEL_WIDTH  = 280;
-local PANEL_HEIGHT = 60;
+
+local INPUT_MESSAGE = "Please enter a name for the repository:";
 
 -- ------------------------------------------------
 -- Module
@@ -38,26 +41,40 @@ function InputPanel.new()
     -- Public Functions
     -- ------------------------------------------------
 
+    ---
+    -- Initialises the InputPanel.
+    -- @param npath  (string) The path to the directory which was dropped on the application window.
+    -- @param params (table)  A table containing the general configuration of LoGiVi.
+    --
     function self:init( npath, params )
         path = npath;
         config = params.config;
     end
 
+    ---
+    -- Draws the InputPanel.
+    --
     function self:draw()
-        local x = love.graphics.getWidth() * 0.5 - PANEL_WIDTH * 0.5;
-        local y = love.graphics.getHeight() * 0.5 - PANEL_HEIGHT * 0.5;
+        local sw, sh = love.graphics.getDimensions();
+
+        local x = sw * 0.5 - PANEL_WIDTH * 0.5;
+        local y = sh * 0.5 - PANEL_HEIGHT * 0.5;
 
         love.graphics.setColor( 0, 0, 0, 200 );
-        love.graphics.rectangle( 'fill' , 0, 0, love.graphics.getDimensions() );
+        love.graphics.rectangle( 'fill' , 0, 0, sw, sh );
         love.graphics.setColor( BG_COLOR );
         love.graphics.rectangle( 'fill', x, y, PANEL_WIDTH, PANEL_HEIGHT, 20, 20, 20 );
         love.graphics.setColor( 255, 255, 255 );
         love.graphics.setFont( INFO_FONT );
-        love.graphics.print( "Please enter a name for the repository:" , x + 20, y + 15 );
+        love.graphics.print( INPUT_MESSAGE , x + 20, y + 15 );
         love.graphics.setFont( LABEL_FONT );
         love.graphics.print( text, x + 20, y + 35 );
     end
 
+    ---
+    -- Handles keypressed events.
+    -- @param key (string) The pressed key.
+    --
     function self:keypressed( key )
         if key == 'escape' then
             ScreenManager.pop();
@@ -71,6 +88,10 @@ function InputPanel.new()
         end
     end
 
+    ---
+    -- Handles textinput events.
+    -- @param txt (string) The user's text input.
+    --
     function self:textinput( txt )
         if index < MAX_LENGTH then
             table.insert( input, index, txt );
@@ -79,6 +100,9 @@ function InputPanel.new()
         end
     end
 
+    ---
+    -- Removes the last character in the input table.
+    --
     function self:remove()
         index = math.max( 1, index - 1 );
         table.remove( input, index );
